320ba1c307a50e131d0afcd1a8ae641614371cbb
Will <whaley@journera.com>
2018-01-23 22:50:04 -0600

    Code clean up

--- app.js
+++ app.js
@@ -11,17 +11,16 @@ var Base64Binary = {
 
 		let arrayBuffer = new ArrayBuffer(bytes);
 
-		this.decode(base64String, arrayBuffer);
+		this.decode(base64String, arrayBuffer, bytes);
 
 		return arrayBuffer;
 	},
 
-	decode: function(base64String, arrayBuffer) {
+	decode: function(base64String, arrayBuffer, bytes) {
 		//get last chars to see if are valid
 		var lkey1 = this._keyStr.indexOf(base64String.charAt(base64String.length-1));
 		var lkey2 = this._keyStr.indexOf(base64String.charAt(base64String.length-1));
 
-		var bytes = Math.ceil((3 * base64String.length) / 4.0);
 		if (lkey1 == 64) bytes--; //padding chars, so skip
 		if (lkey2 == 64) bytes--; //padding chars, so skip
 
@@ -55,12 +54,7 @@ var Base64Binary = {
 	}
 };
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext || null;
-window.OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext || null;
-
-let MIDI = window.MIDI = {};
-MIDI.Soundfont = MIDI.Soundfont || {};
-MIDI.player = MIDI.player || {};
+let MIDI = window.MIDI = { Soundfont: {} };
 
 (function (Context) {
 	var contextMethods = [
@@ -113,68 +107,12 @@ MIDI.player = MIDI.player || {};
 	}
 
 	contextMethods.forEach(function (names) {
-		var name1;
-		var name2;
 		while (names.length) {
-			name1 = names.pop();
-			this[names.pop()] = this[name1];
+			let name = names.pop();
+			this[names.pop()] = this[name];
 		}
 	}, proto);
 })(window.AudioContext);
-
-function asId(name) {
-	return name.replace(/[^a-z0-9_ ]/gi, '').replace(/[ ]/g, '_').toLowerCase();
-};
-
-var GM = (function(arr) {
-	var res = {};
-	var byCategory = res.byCategory = {};
-	var byId = res.byId = {};
-	var byName = res.byName = {};
-	///
-	for (var key in arr) {
-		var list = arr[key];
-		for (var n = 0, length = list.length; n < length; n++) {
-			var instrument = list[n];
-			if (instrument) {
-				var id = parseInt(instrument.substr(0, instrument.indexOf(' ')), 10);
-				var name = instrument.replace(id + ' ', '');
-				var nameId = asId(name);
-				var categoryId = asId(key);
-				///
-				var spec = {
-					id: nameId,
-					name: name,
-					program: --id,
-					category: key
-				};
-				///
-				byId[id] = spec;
-				byName[nameId] = spec;
-				byCategory[categoryId] = byCategory[categoryId] || [];
-				byCategory[categoryId].push(spec);
-			}
-		}
-	}
-	return res;
-})({
-	'Piano': ['1 Acoustic Grand Piano', '2 Bright Acoustic Piano', '3 Electric Grand Piano', '4 Honky-tonk Piano', '5 Electric Piano 1', '6 Electric Piano 2', '7 Harpsichord', '8 Clavinet'],
-	'Chromatic Percussion': ['9 Celesta', '10 Glockenspiel', '11 Music Box', '12 Vibraphone', '13 Marimba', '14 Xylophone', '15 Tubular Bells', '16 Dulcimer'],
-	'Organ': ['17 Drawbar Organ', '18 Percussive Organ', '19 Rock Organ', '20 Church Organ', '21 Reed Organ', '22 Accordion', '23 Harmonica', '24 Tango Accordion'],
-	'Guitar': ['25 Acoustic Guitar (nylon)', '26 Acoustic Guitar (steel)', '27 Electric Guitar (jazz)', '28 Electric Guitar (clean)', '29 Electric Guitar (muted)', '30 Overdriven Guitar', '31 Distortion Guitar', '32 Guitar Harmonics'],
-	'Bass': ['33 Acoustic Bass', '34 Electric Bass (finger)', '35 Electric Bass (pick)', '36 Fretless Bass', '37 Slap Bass 1', '38 Slap Bass 2', '39 Synth Bass 1', '40 Synth Bass 2'],
-	'Strings': ['41 Violin', '42 Viola', '43 Cello', '44 Contrabass', '45 Tremolo Strings', '46 Pizzicato Strings', '47 Orchestral Harp', '48 Timpani'],
-	'Ensemble': ['49 String Ensemble 1', '50 String Ensemble 2', '51 Synth Strings 1', '52 Synth Strings 2', '53 Choir Aahs', '54 Voice Oohs', '55 Synth Choir', '56 Orchestra Hit'],
-	'Brass': ['57 Trumpet', '58 Trombone', '59 Tuba', '60 Muted Trumpet', '61 French Horn', '62 Brass Section', '63 Synth Brass 1', '64 Synth Brass 2'],
-	'Reed': ['65 Soprano Sax', '66 Alto Sax', '67 Tenor Sax', '68 Baritone Sax', '69 Oboe', '70 English Horn', '71 Bassoon', '72 Clarinet'],
-	'Pipe': ['73 Piccolo', '74 Flute', '75 Recorder', '76 Pan Flute', '77 Blown Bottle', '78 Shakuhachi', '79 Whistle', '80 Ocarina'],
-	'Synth Lead': ['81 Lead 1 (square)', '82 Lead 2 (sawtooth)', '83 Lead 3 (calliope)', '84 Lead 4 (chiff)', '85 Lead 5 (charang)', '86 Lead 6 (voice)', '87 Lead 7 (fifths)', '88 Lead 8 (bass + lead)'],
-	'Synth Pad': ['89 Pad 1 (new age)', '90 Pad 2 (warm)', '91 Pad 3 (polysynth)', '92 Pad 4 (choir)', '93 Pad 5 (bowed)', '94 Pad 6 (metallic)', '95 Pad 7 (halo)', '96 Pad 8 (sweep)'],
-	'Synth Effects': ['97 FX 1 (rain)', '98 FX 2 (soundtrack)', '99 FX 3 (crystal)', '100 FX 4 (atmosphere)', '101 FX 5 (brightness)', '102 FX 6 (goblins)', '103 FX 7 (echoes)', '104 FX 8 (sci-fi)'],
-	'Ethnic': ['105 Sitar', '106 Banjo', '107 Shamisen', '108 Koto', '109 Kalimba', '110 Bagpipe', '111 Fiddle', '112 Shanai'],
-	'Percussive': ['113 Tinkle Bell', '114 Agogo', '115 Steel Drums', '116 Woodblock', '117 Taiko Drum', '118 Melodic Tom', '119 Synth Drum'],
-	'Sound effects': ['120 Reverse Cymbal', '121 Guitar Fret Noise', '122 Breath Noise', '123 Seashore', '124 Bird Tweet', '125 Telephone Ring', '126 Helicopter', '127 Applause', '128 Gunshot']
-});
 
 MIDI.getMono = function(channelId) {
 	var channel = channels[channelId];
@@ -200,7 +138,20 @@ MIDI.setMono = function(channelId, truthy, delay) {
 	}
 };
 
-var channels = (function() {
+MIDI.keyToNote = {}; // C8  == 108
+
+(function() {
+	var A0 = 0x15; // first note
+	var C8 = 0x6C; // last note
+	var number2key = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+	for (var n = A0; n <= C8; n++) {
+		var octave = (n - 12) / 12 >> 0;
+		var name = number2key[n % 12] + octave;
+		MIDI.keyToNote[name] = n;
+	}
+})();
+
+MIDI.channels = (function() {
 	var res = {};
 	for (var number = 0; number <= 15; number++) {
 		res[number] = {
@@ -216,53 +167,27 @@ var channels = (function() {
 	return res;
 })();
 
-MIDI.keyToNote = {}; // C8  == 108
-MIDI.noteToKey = {}; // 108 ==  C8
-
-(function() {
-	var A0 = 0x15; // first note
-	var C8 = 0x6C; // last note
-	var number2key = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-	for (var n = A0; n <= C8; n++) {
-		var octave = (n - 12) / 12 >> 0;
-		var name = number2key[n % 12] + octave;
-		MIDI.keyToNote[name] = n;
-		MIDI.noteToKey[n] = name;
-	}
-})();
-
-MIDI.channels = channels;
-MIDI.GM = GM;
-
 const API = 'webaudio'; // 'webmidi', 'audiotag'
 const FORMAT = 'ogg';
 
 MIDI.loadPlugin = function(opts = {}) {
 	return new Promise((resolve) => {
-		connect[API](opts);
-
-		resolve();
-	});
-};
-
-const connect = {
-	webaudio: function(opts) {
 		const context = 'WebAudio';
 
-		if (MIDI.Soundfont[opts.instrument]) { // already loaded
+		if (MIDI.Soundfont[opts.instrument]) {
 			MIDI[context].connect(opts);
 
-			return;
+			return resolve();
 		}
 
-		// needs to be requested
 		sendRequest(opts.instrument, FORMAT).then(() => {
 			MIDI[context].connect(opts);
-
 		}).catch((e) => {
 			console.error(e);
 		});
-	}
+
+		resolve();
+	});
 };
 
 function sendRequest(programId) {
@@ -285,16 +210,10 @@ function sendRequest(programId) {
 	});
 };
 
-MIDI.setDefaultPlugin = function(midi) {
-	for (var key in midi) {
-		MIDI[key] = midi[key];
-	}
-};
-
-window.AudioContext && (function() {
+(function() {
 	var audioContext = null; // new AudioContext();
 	var midi = MIDI.WebAudio = {api: 'webaudio'};
-	var ctx; // audio context
+	var midiAudioContext; // audio context
 	var sources = {};
 	var masterVolume = 127;
 	var audioBuffers = {};
@@ -350,19 +269,19 @@ window.AudioContext && (function() {
 		}
 
 		// convert relative delay to absolute delay
-		if (delay < ctx.currentTime) {
-			delay += ctx.currentTime;
+		if (delay < midiAudioContext.currentTime) {
+			delay += midiAudioContext.currentTime;
 		}
 
-		var source = ctx.createBufferSource();
+		var source = midiAudioContext.createBufferSource();
 		source.buffer = buffer;
 
 		/// add gain + pitchShift
 		var gain = (velocity / 127) * (masterVolume / 127) * 2 - 1;
-		source.connect(ctx.destination);
+		source.connect(midiAudioContext.destination);
 		source.playbackRate.value = 1; // pitch shift
-		source.gainNode = ctx.createGain(); // gain
-		source.gainNode.connect(ctx.destination);
+		source.gainNode = midiAudioContext.createGain(); // gain
+		source.gainNode.connect(midiAudioContext.destination);
 		source.gainNode.gain.value = Math.min(1.0, Math.max(-1.0, gain));
 		source.connect(source.gainNode);
 		source.start(delay || 0);
@@ -381,8 +300,8 @@ window.AudioContext && (function() {
 		var bufferId = instrument + 'x' + noteId;
 		var buffer = audioBuffers[bufferId];
 		if (buffer) {
-			if (delay < ctx.currentTime) {
-				delay += ctx.currentTime;
+			if (delay < midiAudioContext.currentTime) {
+				delay += midiAudioContext.currentTime;
 			}
 			///
 			var source = sources[channelId + 'x' + noteId];
@@ -409,21 +328,20 @@ window.AudioContext && (function() {
 		}
 	};
 
-	midi.connect = function(opts) {
-		MIDI.setDefaultPlugin(midi);
-		midi.setContext(createAudioContext(), opts);
-	};
+	midi.connect = function() {
+		// TODO WFH This is insane.
+		for (var key in midi) {
+			MIDI[key] = midi[key];
+		}
 
-	midi.setContext = function(newCtx) {
-		ctx = newCtx;
+		midiAudioContext = new window.AudioContext();
 
-		/// loading audio files
-		var urls = [];
+		var keyURLs = [];
 		var notes = MIDI.keyToNote;
 		for (var key in notes) {
-			urls.push(key);
+			keyURLs.push(key);
 		}
-		///
+
 		function waitForEnd(instrument) {
 			for (var key in bufferPending) { // has pending items
 				if (bufferPending[key]) {
@@ -433,14 +351,14 @@ window.AudioContext && (function() {
 		};
 
 		function requestAudio(soundfont, programId, index, key) {
-			var url = soundfont[key];
-			if (!url) {
+			var keyURL = soundfont[key];
+			if (!keyURL) {
 				return;
 			}
 
 			bufferPending[programId] ++;
 
-			loadAudio(url).then((buffer) => {
+			loadAudio(keyURL).then((buffer) => {
 				buffer.id = key;
 				var noteId = MIDI.keyToNote[key];
 				audioBuffers[programId + 'x' + noteId] = buffer;
@@ -463,16 +381,16 @@ window.AudioContext && (function() {
 				continue;
 			}
 
-			var spec = MIDI.GM.byName[instrument];
-			if (spec) {
-				var programId = spec.program;
-				///
-				bufferPending[programId] = 0;
-				///
-				for (var index = 0; index < urls.length; index++) {
-					var key = urls[index];
-					requestAudio(soundfont, programId, index, key);
-				}
+			const spec = {
+				id:      'tuba',
+				name:    'Tuba',
+				program:  58,
+			};
+
+			bufferPending[spec.program] = 0;
+
+			for (var index = 0; index < keyURLs.length; index++) {
+				requestAudio(soundfont, spec.program, index, keyURLs[index]);
 			}
 		}
 		///
@@ -481,15 +399,9 @@ window.AudioContext && (function() {
 
 	function loadAudio(url) {
 		return new Promise((resolve, reject) => {
-			console.log('loadAudio');
-
 			var base64 = url.split(',')[1];
 			var buffer = Base64Binary.decodeArrayBuffer(base64);
-			ctx.decodeAudioData(buffer, resolve, reject);
+			midiAudioContext.decodeAudioData(buffer, resolve, reject);
 		});
-	};
-
-	function createAudioContext() {
-		return new (window.AudioContext || window.webkitAudioContext)();
 	};
 })();
